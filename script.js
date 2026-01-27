afc5e4b2abeb702de4b177cd37387376aba871d2
Abdul Rehman <71893203+Git-EXPifani@users.noreply.github.com>
2026-01-27 22:46:43 +0500

    Made IIFE functions for Gameboard and displayBoard

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function hehe() {
-  return hehe;
-}
+const GameBoard = (function () {
+  const board = Array(9).fill(" ");
+
+  const getBoard = () => board;
+
+  const setMark = (index, mark) => {
+    if (board[index] !== " ") return false;
+    board[index] = mark;
+    return true;
+  };
+
+  return { setMark, getBoard };
+})();
+
+const displayBoard = () => {
+  const board = GameBoard.getBoard();
+
+  console.log(`
+      ${board[0]} | ${board[1]} | ${board[2]}
+      ____________
+      ${board[3]} | ${board[4]} | ${board[5]}
+      ____________
+      ${board[6]} | ${board[7]} | ${board[8]}
+      ____________
+    `);
+};
